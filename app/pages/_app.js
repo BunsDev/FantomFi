2a7834954649b7b2eaad805674677df8c3d0b730
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2023-03-08 13:44:05 +0530

    feat: New Fantom Testnet network added

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -9,54 +9,31 @@ import { configureChains, createClient, goerli, WagmiConfig } from "wagmi";
 // import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 
-const xdcApothem = {
-  id: 51,
-  name: "Apothem-Network (TestNet)",
-  network: "XDC Apothem Network (TestNet)",
+const fantomTestnet = {
+  id: 4002,
+  name: "Fantom testnet",
+  network: "Fantom testnet",
   nativeCurrency: {
     decimals: 18,
-    name: "XDC-Network",
-    symbol: "XDC",
+    name: "Test FTM",
+    symbol: "FTM",
   },
   rpcUrls: {
     default: {
-      http: ["https://erpc.apothem.network"],
+      http: ["https://rpc.ankr.com/fantom_testnet"],
     },
   },
   blockExplorers: {
     default: {
-      name: "Apothem Explorer",
-      url: "https://explorer.apothem.network/",
+      name: "Fantom Explorer",
+      url: "https://testnet.ftmscan.com",
     },
   },
   testnet: true,
 };
 
-const xdcMainnet = {
-  id: 50,
-  name: "Xinfin MainNet",
-  network: "Xinfin Network MainNet",
-  nativeCurrency: {
-    decimals: 18,
-    name: "XDC Network",
-    symbol: "XDC",
-  },
-  rpcUrls: {
-    default: {
-      http: ["https://erpc.apothem.network"],
-    },
-  },
-  blockExplorers: {
-    default: {
-      name: "Xinfin Scan",
-      url: "https://explorer.xinfin.network/",
-    },
-  },
-  testnet: false,
-};
-
 const { provider, chains } = configureChains(
-  [xdcApothem],
+  [fantomTestnet],
   [
     // alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
     publicProvider(),
